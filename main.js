733e17f8036e9fee2373828371a7a7fba5ae4eea
Jesse Soldat <jsoldat@hotmail.com>
2016-01-09 10:51:46 -0500

    set up an array of 4 questions. created the getElem function for getting elements in the HTML document.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//ALPHABET
 var btns = "";
 var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var letterArray = letters.split("");
@@ -11,10 +12,29 @@ for(var i = 0; i < 26; i++) {
     } else {
  
     btns += '<button class="mybtns lightblue" onclick="alert(\''+letter+'\');">'+letter+'</button>';
-  }
-
-
+  } 
 }
-document.write(btns);
+var alpha = document.getElementById('alpha');
+alpha.innerHTML = btns;
+// document.write(btns);
+
+//QUIZ
+var test, test_status, question, choice, choices, chA, chB, chC;
+var pos = 0;
+var correct = 0;
+var questions = [
+    ['What is 25 / 5 ?', '4,', '7', '5', '6', 'C'],
+    ['What is 7 * 3 ?', '24,', '21', '32', '26', 'B'],
+    ['What is 30 / 3 ?', '10,', '8', '12', '9', 'A'],
+    ['What is 9 * 9 ?', '79,', '84', '81', '86', 'C']
+];
+function getElem(elem) {
+  return document.getElementById(elem);
+}
+function renderQuestion() {
+  test = getElem('test');
+  testStatus = getElem('testStatus').innerHTML = 'Question '+(pos+1)+' of '+questions.length;
+}
+renderQuestion();
 
 
