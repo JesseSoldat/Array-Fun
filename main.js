42207dd62cb4fa73e98bb792dfbac76ce54b6dab
Jesse Soldat <jsoldat@hotmail.com>
2016-01-09 11:27:51 -0500

    created a check answer function that returns the value of what radio button was submitted

--- main.js
+++ main.js
@@ -23,10 +23,10 @@ var test, test_status, question, choice, choices, chA, chB, chC, chD;
 var pos = 0;
 var correct = 0;
 var questions = [
-    ['What is 25 / 5 ?', '4,', '7', '5', '6', 'C'],
-    ['What is 7 * 3 ?', '24,', '21', '32', '26', 'B'],
-    ['What is 30 / 3 ?', '10,', '8', '12', '9', 'A'],
-    ['What is 9 * 9 ?', '79,', '84', '81', '86', 'C']
+    ['What is 25 / 5 ?', '4', '7', '5', '6', 'C'],
+    ['What is 7 * 3 ?', '24', '21', '32', '26', 'B'],
+    ['What is 30 / 3 ?', '10', '8', '12', '9', 'A'],
+    ['What is 9 * 9 ?', '79', '84', '81', '86', 'C']
 ];
 function getElem(elem) {
   return document.getElementById(elem);
@@ -40,8 +40,27 @@ function renderQuestion() {
   chC = questions[pos][3];
   chD = questions[pos][4];
   test.innerHTML = '<h3>'+question+'</h3>';
+  test.innerHTML += '<input type="radio" name="choices" value="A">';
+  test.innerHTML += '<label>'+chA+'</label><br>';
+  test.innerHTML += '<input type="radio" name="choices" value="B">';
+  test.innerHTML += '<label>'+chB+'</label><br>';
+  test.innerHTML += '<input type="radio" name="choices" value="C">';
+  test.innerHTML += '<label>'+chC+'</label><br>';
+  test.innerHTML += '<input type="radio" name="choices" value="D">';
+  test.innerHTML += '<label>'+chD+'</label><br>';
+  test.innerHTML += '<button class="submitBtn" onclick="checkAnswer()">Submit</button>';
+}
+function checkAnswer() {
+  choices = document.getElementsByName('choices');
+  for(var i = 0; i < choices.length; i++) {
+    if(choices[i].checked) {
+      choice = choices[i].value;
+      console.log(choice);
+    }
+  }
 
 }
-renderQuestion();
+window.addEventListener("load", renderQuestion, false);
+
 
 
