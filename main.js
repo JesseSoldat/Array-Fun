3e76f3ceac49c1897f8f106f9c62000fe068e5ca
Jesse Soldat <jsoldat@hotmail.com>
2016-01-09 11:54:28 -0500

    got the check answer feature to work

--- main.js
+++ main.js
@@ -23,22 +23,30 @@ var test, test_status, question, choice, choices, chA, chB, chC, chD;
 var pos = 0;
 var correct = 0;
 var questions = [
-    ['What is 25 / 5 ?', '4', '7', '5', '6', 'C'],
-    ['What is 7 * 3 ?', '24', '21', '32', '26', 'B'],
-    ['What is 30 / 3 ?', '10', '8', '12', '9', 'A'],
-    ['What is 9 * 9 ?', '79', '84', '81', '86', 'C']
+    ['What is 25 / 5 ?', '4', '7', '5', 'C'],
+    ['What is 7 * 3 ?', '24', '21', '32', 'B'],
+    ['What is 30 / 3 ?', '10', '8', '12', 'A'],
+    ['What is 9 * 9 ?', '79', '84', '81', 'C']
 ];
+
 function getElem(elem) {
   return document.getElementById(elem);
 }
 function renderQuestion() {
   test = getElem('test');
+
+  if(pos >= questions.length) {
+    test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>"; 
+    getElem("testStatus").innerHTML = "Test Complete";
+    pos = 0;
+    correct = 0;
+    return false;
+  }
   testStatus = getElem('testStatus').innerHTML = 'Question '+(pos+1)+' of '+questions.length;
   question = questions[pos][0];
   chA = questions[pos][1];
   chB = questions[pos][2];
   chC = questions[pos][3];
-  chD = questions[pos][4];
   test.innerHTML = '<h3>'+question+'</h3>';
   test.innerHTML += '<input type="radio" name="choices" value="A">';
   test.innerHTML += '<label>'+chA+'</label><br>';
@@ -46,18 +54,22 @@ function renderQuestion() {
   test.innerHTML += '<label>'+chB+'</label><br>';
   test.innerHTML += '<input type="radio" name="choices" value="C">';
   test.innerHTML += '<label>'+chC+'</label><br>';
-  test.innerHTML += '<input type="radio" name="choices" value="D">';
-  test.innerHTML += '<label>'+chD+'</label><br>';
   test.innerHTML += '<button class="submitBtn" onclick="checkAnswer()">Submit</button>';
 }
 function checkAnswer() {
   choices = document.getElementsByName('choices');
+
   for(var i = 0; i < choices.length; i++) {
     if(choices[i].checked) {
       choice = choices[i].value;
-      console.log(choice);
+      // console.log(choice);
     }
-  }
+  } //for
+  if(choice == questions[pos][4]){
+      correct++;
+    }
+  pos++;
+  renderQuestion();
 
 }
 window.addEventListener("load", renderQuestion, false);
